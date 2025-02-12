1f047a6e6a2a7d79e4e442b2cb86cbb03c082189
wudijun <95115088+wudijun@users.noreply.github.com>
2025-02-12 16:04:00 +0800

    Update http.go

--- finger/http.go
+++ finger/http.go
@@ -13,18 +13,19 @@ import (
 	"time"
 )
 
-func xegexpjs(reg string, resp string) (reslut1 [][]string) {
+func xegexpjs(reg string, resp string) (result1 [][]string) {
 	reg1 := regexp.MustCompile(reg)
 	if reg1 == nil {
 		log.Println("regexp err")
 		return nil
 	}
-	result1 := reg1.FindAllStringSubmatch(resp, -1)
+	result1 = reg1.FindAllStringSubmatch(resp, -1)
 	return result1
 }
 
 func rndua() string {
-	ua := []string{"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 YaBrowser/22.1.0.2517 Yowser/2.5 Safari/537.36",
+	ua := []string{
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 YaBrowser/22.1.0.2517 Yowser/2.5 Safari/537.36",
 		"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0",
 		"Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0",
 		"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
@@ -37,8 +38,9 @@ func rndua() string {
 		"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0",
 		"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
 		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Safari/605.1.15",
-		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 YaBrowser/22.1.0.2517 Yowser/2.5 Safari/537.36"}
-	n := rand.Intn(13) + 1
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 YaBrowser/22.1.0.2517 Yowser/2.5 Safari/537.36",
+	}
+	n := rand.Intn(len(ua))
 	return ua[n]
 }
 
@@ -48,56 +50,31 @@ func Gettitle(httpbody string) string {
 		return "Not found"
 	}
 	title := doc.Find("title").Text()
-	title = strings.Replace(title, "\n", "", -1)
-	title = strings.Trim(title, " ")
+	title = strings.ReplaceAll(title, "\n", "")
+	title = strings.TrimSpace(title)
 	return title
 }
 
-//	func Getfavicon(httpbody string, turl string) string {
-//		faviconpaths := xegexpjs(`href="(.*?favicon....)"`, httpbody)
-//		var faviconpath string
-//		_, err := url.Parse(turl)
-//		if err != nil {
-//			panic(err)
-//		}
-//		turl = turl + "/"
-//		if len(faviconpaths) > 0 {
-//			fav := faviconpaths[0][1]
-//			if fav[:2] == "//" {
-//				faviconpath = "http:" + fav
-//			} else {
-//				if fav[:4] == "http" {
-//					faviconpath = fav
-//				} else {
-//					faviconpath = turl + fav
-//				}
-//
-//			}
-//		} else {
-//			faviconpath = turl + "favicon.ico"
-//		}
-//		return favicohash(faviconpath)
-//	}
 func Getfavicon(httpBody string, turl string) string {
 	faviconPaths := xegexpjs(`href="(.*?favicon....)"`, httpBody)
 	var faviconPath string
 	_, err := url.Parse(turl)
 	if err != nil {
-		panic(err)
+		//panic(err)
 	}
 	turl = strings.TrimRight(turl, "/") // 移除末尾的斜杠
-
 	if len(faviconPaths) > 0 {
 		fav := faviconPaths[0][1]
 		if strings.HasPrefix(fav, "//") {
-			faviconPath = "http:" + fav
+			faviconPath = "http:" + fav // 或者 "https:"，取决于需求
 		} else if strings.HasPrefix(fav, "http") {
 			faviconPath = fav
 		} else {
-			faviconPath = turl + "/" + fav
+			// 确保拼接时没有多余的斜杠
+			faviconPath = strings.TrimRight(turl, "/") + "/" + strings.TrimLeft(fav, "/")
 		}
 	} else {
-		faviconPath = turl + "/favicon.ico"
+		faviconPath = strings.TrimRight(turl, "/") + "/favicon.ico"
 	}
 	return favicohash(faviconPath)
 }
@@ -110,112 +87,81 @@ func Httprequest(domain string) (string, string, http.Header, int, int, string, 
 		Timeout:   10 * time.Second,
 		Transport: tr,
 	}
-	//看域名是否有协议，如果是http则直接请求，如果不是http则判断是否是https
-	if strings.HasPrefix(domain, "http://") {
-		req, err := http.NewRequest("GET", domain, nil)
+
+	makeRequest := func(url string) (string, http.Header, int, int) {
+		req, err := http.NewRequest("GET", url, nil)
 		if err != nil {
-			//fmt.Println("1Error creating request:", err)
-			return "", "", nil, 0, 0, "", ""
+			return "", nil, 0, 0
 		}
 		req.Header.Set("User-Agent", rndua())
-		cookie := &http.Cookie{
-			Name:  "rememberMe",
-			Value: "me",
-		}
 		req.Header.Set("Accept", "*/*;q=0.8")
 		req.Header.Set("Connection", "close")
-		req.AddCookie(cookie)
-		req.Header.Set("User-Agent", rndua())
-		http_response, http_err := client.Do(req)
-		if http_err != nil {
-			return "", "", nil, 0, 0, "", ""
+		//cookie := &http.Cookie{
+		//	Name:  "rememberMe",
+		//	Value: "me",
+		//}
+		//req.AddCookie(cookie)
+		httpResponse, httpErr := client.Do(req)
+		if httpErr != nil {
+			return "", nil, 0, 0
 		}
-		htmlContent, _ := ioutil.ReadAll(http_response.Body)
-		htmlheaders := http_response.Header
-		defer http_response.Body.Close()
-		title := Gettitle(string(htmlContent))
-		return title, string(htmlContent), htmlheaders, http_response.StatusCode, len(string(htmlContent)), Getfavicon(string(htmlContent), domain), ""
-	} else if strings.HasPrefix(domain, "https://") {
-		//看域名是否以https开头，如果是则直接请求
-		req, err := http.NewRequest("GET", domain, nil)
-		if err != nil {
-			//fmt.Println("1Error creating request:", err)
-			return "", "", nil, 0, 0, "", ""
-		}
-		req.Header.Set("User-Agent", rndua())
-		cookie := &http.Cookie{
-			Name:  "rememberMe",
-			Value: "me",
-		}
-		req.Header.Set("Accept", "*/*;q=0.8")
-		req.Header.Set("Connection", "close")
-		req.AddCookie(cookie)
-		req.Header.Set("User-Agent", rndua())
-		http_response, http_err := client.Do(req)
-		if http_err != nil {
-			return "", "", nil, 0, 0, "", ""
-		}
-		htmlContent, _ := ioutil.ReadAll(http_response.Body)
-		htmlheaders := http_response.Header
-		defer http_response.Body.Close()
-		title := Gettitle(string(htmlContent))
-		return title, string(htmlContent), htmlheaders, http_response.StatusCode, len(string(htmlContent)), Getfavicon(string(htmlContent), domain), ""
-	} else {
-		//如果既不是以http开头也不是https开头则证明没有写，那么先请求https再请求http
-		req, err := http.NewRequest("GET", "https://"+domain, nil)
-		if err != nil {
-			//fmt.Println("1Error creating request:", err)
-			return "", "", nil, 0, 0, "", ""
-		}
-		req.Header.Set("User-Agent", rndua())
-		cookie := &http.Cookie{
-			Name:  "rememberMe",
-			Value: "me",
-		}
-		req.Header.Set("Accept", "*/*;q=0.8")
-		req.Header.Set("Connection", "close")
-		req.AddCookie(cookie)
-		req.Header.Set("User-Agent", rndua())
-		http_response, http_err := client.Do(req)
-
-		if http_err != nil {
-			req, err := http.NewRequest("GET", "http://"+domain, nil)
-			if err != nil {
-				//fmt.Println("2Error creating request:", err)
-				return "", "", nil, 0, 0, "", ""
-			}
-			cookie := &http.Cookie{
-				Name:  "rememberMe",
-				Value: "me",
-			}
-			req.Header.Set("Accept", "*/*;q=0.8")
-			req.Header.Set("Connection", "close")
-			req.AddCookie(cookie)
-			req.Header.Set("User-Agent", rndua())
-			http_response, http_err := client.Do(req)
-
-			if http_err != nil {
-				//fmt.Println("3Error creating request:", http_err)
-				return "", "", nil, 0, 0, "", ""
-			}
-			//获取响应体
-			htmlContent, _ := ioutil.ReadAll(http_response.Body)
-			//fmt.Println(string(htmlContent))
-			//获取响应头
-			htmlheaders := http_response.Header
-
-			defer http_response.Body.Close()
-			title := Gettitle(string(htmlContent))
-			return title, string(htmlContent), htmlheaders, http_response.StatusCode, len(string(htmlContent)), Getfavicon(string(htmlContent), "http://"+domain), "http://"
-		}
-		//读取响应的body内容
-		htmlContent, _ := ioutil.ReadAll(http_response.Body)
-		//fmt.Println(string(htmlContent))
-		//获取响应头
-		htmlheaders := http_response.Header
-		defer http_response.Body.Close()
-		title := Gettitle(string(htmlContent))
-		return title, string(htmlContent), htmlheaders, http_response.StatusCode, len(string(htmlContent)), Getfavicon(string(htmlContent), "https://"+domain), "https://"
+		htmlContent, _ := ioutil.ReadAll(httpResponse.Body)
+		defer httpResponse.Body.Close()
+		return string(htmlContent), httpResponse.Header, httpResponse.StatusCode, len(htmlContent)
 	}
 
+	parseAndFollowRedirect := func(htmlContent, baseURL string) (string, http.Header, int, int) {
+		redirectPatterns := []string{
+			`window\.parent\.location\.href\s*=\s*['"](.*?)['"];`,
+			`window\.location\.href\s*=\s*['"](.*?)['"];`,
+			`window\.top\.location\s*=\s*['"](.*?)['"];`,
+			`window\.location\s*=\s*['"](.*?)['"];`,
+			`location\.href\s*=\s*['"](.*?)['"];`,
+			`location\s*=\s*['"](.*?)['"];`,
+			`eval\("window\.".*?\.location\s*=\s*['"](.*?)['"]\);`,
+			`<meta\s+http-equiv=["']refresh["']\s+content=["']\d*;\s*url=(.*?)["']`,
+		}
+
+		for _, pattern := range redirectPatterns {
+			redirectRegex := regexp.MustCompile(pattern)
+			matches := redirectRegex.FindStringSubmatch(htmlContent)
+			if len(matches) > 1 {
+				redirectURL := matches[1]
+				if !strings.HasPrefix(redirectURL, "http") {
+					base, err := url.Parse(baseURL)
+					if err != nil {
+						return "", nil, 0, 0
+					}
+					redirectURL = base.ResolveReference(&url.URL{Path: redirectURL}).String()
+				}
+				return makeRequest(redirectURL)
+			}
+		}
+		return htmlContent, nil, 0, 0
+	}
+
+	var htmlContent string
+	var htmlHeader http.Header
+	var statusCode, contentLength int
+
+	if strings.HasPrefix(domain, "http://") || strings.HasPrefix(domain, "https://") {
+		htmlContent, htmlHeader, statusCode, contentLength = makeRequest(domain)
+	} else {
+		htmlContent, htmlHeader, statusCode, contentLength = makeRequest("https://" + domain)
+		if statusCode == 0 {
+			htmlContent, htmlHeader, statusCode, contentLength = makeRequest("http://" + domain)
+		}
+	}
+
+	title := Gettitle(htmlContent)
+	favicon := Getfavicon(htmlContent, domain)
+
+	if statusCode == 0 {
+		// 处理跳转
+		htmlContent, htmlHeader, statusCode, contentLength = parseAndFollowRedirect(htmlContent, domain)
+		title = Gettitle(htmlContent)
+		favicon = Getfavicon(htmlContent, domain)
+	}
+	//print(htmlContent)
+	return title, htmlContent, htmlHeader, statusCode, contentLength, favicon, ""
 }
